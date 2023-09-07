4383b92b0508e62349beac6f627a64eafdc19bc1
James Cuzella <james.cuzella@lyraphase.com>
2023-09-07 17:50:27 -0600

    Example: Return Values and Scope

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,9 @@ fn main() {
 
     // Ownership and Functions
     ownership_and_functions();
+
+    // Return Values and Scope
+    return_values_and_scope();
 }
 
 // Example: The String Type
@@ -103,3 +106,36 @@ fn makes_copy(some_integer: i32) {
     // some_integer comes into scope
     println!("some_integer: {}", some_integer);
 } // Here, some_integer goes out of scope. Nothing special happens.
+
+// Example: Return Values and Scope
+fn return_values_and_scope() {
+    let s1 = gives_ownership(); // gives_ownership moves its return
+                                // value into s1
+
+    let s2 = String::from("hello"); // s2 comes into scope
+
+    let s3 = takes_and_gives_back(s2); // s2 is moved into
+                                       // takes_and_gives_back, which also
+                                       // moves its return value into s3
+} // Here, s3 goes out of scope and is dropped. s2 was moved, so nothing
+  // happens. s1 goes out of scope and is dropped.
+
+fn gives_ownership() -> String {
+    // gives_ownership will move its
+    // return value into the function
+    // that calls it
+
+    let some_string = String::from("yours"); // some_string comes into scope
+
+    some_string // some_string is returned and
+                // moves out to the calling
+                // function
+}
+
+// This function takes a String and returns one
+fn takes_and_gives_back(a_string: String) -> String {
+    // a_string comes into
+    // scope
+
+    a_string // a_string is returned and moves out to the calling function
+}
