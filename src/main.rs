3ef44877d48d6d1893211a47be08e730d972e6e5
James Cuzella <james.cuzella@lyraphase.com>
2023-09-07 16:48:54 -0600

    Example: The String Type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    // Scope example
+    {
+        // s is not valid here, it’s not yet declared
+        let s = "hello"; // s is valid from this point forward
+
+        // do stuff with s
+        println!("variable `s` is: {s}");
+    } // this scope is now over, and s is no longer valid
+
+    // The String Type
+    strings();
+}
+
+// Example: The String Type
+fn strings() {
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{}", s); // This will print `hello, world!`
 }
