56270a1829b811e46fbb2b567ab7e71ff53e6a96
James Cuzella <james.cuzella@lyraphase.com>
2023-09-07 17:17:17 -0600

    Example: Variables and Data Interacting with Move

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ fn main() {
 
     // The String Type
     strings();
+
+    // Variables and Data Interacting with Move
+    stack_vs_heap();
 }
 
 // Example: The String Type
@@ -20,3 +23,21 @@ fn strings() {
 
     println!("{}", s); // This will print `hello, world!`
 }
+
+// Example: Variables and Data Interacting with Move
+fn stack_vs_heap() {
+    // Size is known at compile-time, so stored on the Stack
+    let x = 5;
+    let y = x;
+    println!("`y` is stack-allocated: {y}");
+
+    // String type is growable
+    // Size is unknown at compile-time, so must be Heap-allocated
+    let s1 = String::from("hello");
+    let s2 = s1; // s1 is "moved": s2 copied s1's internal values: ptr, len, capacity
+
+    // now s1 is considered invalid, because it was "moved"
+    // println!("{}, world!", s1); // borrow of moved value: `s1`
+    println!("{}, world!", s2);
+
+} // s2 goes out of scope, and Heap memory is freed / drop()
